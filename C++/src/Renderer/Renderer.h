f3c99098c00cb85901d89bca97cc0fce1f1ebac5
Nero <109078102+Nero551@users.noreply.github.com>
2026-06-26 10:52:34 +0300

    removed the openGL project into its own repo

--- C++/src/Renderer/Renderer.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../Math/Math.h"
-
-struct Vertex{
-    Vector3 Position;
-    Vertex(Vector3 position);
-};
--- C++/src/Renderer/Vertex.cpp
+++
@@ -1,4 +0,0 @@
-#include "Renderer.h"
-#include "../Math/Math.h"
-
-Vertex::Vertex(Vector3 pos) : Position(pos) {}
